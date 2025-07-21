963e7b668220e2ab3e779f56c286f7c1d099341d
spreadneck <nate@spreadneck.com>
2025-07-21 12:13:54 -0500

    add: mini.nvim

--- lua/spreadneck/plugins/snacks.nvim.lua
+++ lua/spreadneck/plugins/snacks.nvim.lua
@@ -5,9 +5,6 @@ return {
     event = "VeryLazy",
     ---@type snacks.Config
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
       bigfile = { enabled = true },
       dashboard = { enabled = true },
       explorer = { enabled = true },
@@ -20,6 +17,12 @@ return {
       scroll = { enabled = true },
       statuscolumn = { enabled = true },
       words = { enabled = true },
+      lazygit = { enabled = true },
     },
+    config = function()
+      vim.keymap.set("n", "<leader>gg", function()
+        Snacks.lazygit()
+      end, { desc = "Toggle LazyGit" })
+    end,
   },
 }
